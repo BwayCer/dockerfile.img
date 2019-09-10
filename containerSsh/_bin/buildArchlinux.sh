7fd2f47172f76f9631085ae38639f504ec036b56
BwayCer <bwaycer@gmail.com>
2019-09-10 23:50:18 +0800

    優化 拆分安全殼映像文件的函式庫的更改

--- containerSsh/_bin/buildArchlinux.sh
+++ containerSsh/_bin/buildArchlinux.sh
@@ -9,65 +9,7 @@ __filename=`realpath "$0"`
 _dirsh=`dirname "$__filename"`
 
 source "$_dirsh/build.lib.sh"
-
-
-##shStyle 介面函式
-
-
-fnBuild() {
-    local opt_update=0
-
-    while [ -n "y" ]
-    do
-        case "$1" in
-            -u | --update )
-                opt_update=1
-                shift
-                ;;
-            * ) break ;;
-        esac
-    done
-
-    fnBuild_log "-> cd \"$projectDir\""
-    cd "$projectDir"
-
-    local archlinuxVmfile="$vmFileDirName/archlinux.dockerfile"
-    local archlinuxImgName="local/containerssh:archlinux"
-
-    local imgMethod
-    [ $opt_update -eq 0 ] && imgMethod="check:Image" || imgMethod="check:ImageUpdate"
-    fnBuild_checkDocker "check:docker" \
-        "check:vmfile" "$archlinuxVmfile" \
-        "$imgMethod" "$archlinuxImgName"
-
-    fnSshKeyGenerator
-    fnBuild_run --network host -t "$archlinuxImgName" -f "$archlinuxVmfile" .
-}
-
-
-##shStyle 函式庫
-
-
-fnSshKeyGenerator() {
-    local typeName cmdList
-    local srcDirPath="$vmFileDirName/_src/sshKey"
-
-    [ -d "$srcDirPath" ] && return
-
-    cmdList=("mkdir" "-p" "$srcDirPath")
-    fnBuild_log "-> ${cmdList[*]}"
-    "${cmdList[@]}"
-
-    for typeName in rsa dsa ecdsa ed25519
-    do
-        cmdList=("ssh-keygen" "-t" "$typeName" "-f" "$srcDirPath/ssh_host_${typeName}_key" "-P" "")
-        fnBuild_log "-> ${cmdList[*]}"
-        "${cmdList[@]}"
-    done
-}
-
-
-##shStyle ###
+source "$_dirsh/buildSsh.lib.sh" "archlinux"
 
 
 fnBuild "$@"
