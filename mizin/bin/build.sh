6862fc968a0b039d496dcc2c597246fb98446914
BwayCer <bwaycer@gmail.com>
2020-05-03 19:19:12 +0800

    feat: mizarch, mizin 的安全殼入口的功能

--- mizin/bin/build.sh
+++ mizin/bin/build.sh
@@ -10,6 +10,7 @@ _shBase_loadfile="$projectDir/bin/build.lib.sh"
     source "$projectDir/bin/build.lib.sh" ||
     source <(curl "https://raw.githubusercontent.com/BwayCer/image.docker/tree/master/bin/build.lib.sh")
 
-fnBuild "$@" -t "local/mizin:bwaycer" -f "$projectDir/bwaycer.dockerfile" "$projectDir"
-fnBuild "$@" -t "local/mizin:latest"  -f "$projectDir/Dockerfile"         "$projectDir"
+fnBuild "$@" -t "local/mizin:bwaycer"  -f "$projectDir/bwaycer.dockerfile"  "$projectDir"
+fnBuild "$@" -t "local/mizin:sshentry" -f "$projectDir/sshentry.dockerfile" "$projectDir"
+fnBuild "$@" -t "local/mizin:latest"   -f "$projectDir/Dockerfile"          "$projectDir"
 
